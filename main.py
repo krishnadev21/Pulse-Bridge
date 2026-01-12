7b40d67c65312e3001c4e9c42662eb9af76c7fd4
mohan17032003 <mohankrishnanda007@gmail.com>
2026-01-12 23:02:51 +0530

    Implementing Group Presence Socket

--- main.py
+++ main.py
@@ -398,7 +398,7 @@ async def presence_socket(websocket: WebSocket, user_id: int):
         try:
             print(f"Removing connection for user {user_id}, client {client_id}")
             await PresenceManager.remove_connection(user_id, client_id)
-            print(f" ----------------------==============-? {user_connections} % {user_presence}")
+            print(f" ----------------------============== -? {user_connections} % {user_presence}")
         except Exception as e:
             print(f"Error removing connection for user {user_id}: {e}")
 
